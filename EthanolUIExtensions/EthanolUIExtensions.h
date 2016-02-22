4eb2ad24445d0b2e8ffccb9657b3a04fca76d823
Stephane Copin <stephane@fueled.com>
2016-02-22 18:26:21 -0500

    feat(placeholderTextView): remove placeholder text view from ethanol-ui-extensions

--- EthanolUIExtensions/EthanolUIExtensions.h
+++ EthanolUIExtensions/EthanolUIExtensions.h
@@ -16,5 +16,4 @@ FOUNDATION_EXPORT const unsigned char EthanolUIExtensionsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <EthanolUIExtensions/PublicHeader.h>
 
-#import "ETHPlaceholderTextView.h"
 #import "MKMapView+ZoomLevel.h"
